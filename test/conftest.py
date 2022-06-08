138e0a106ae207fc9b2cc1a81a0c2b36eaea9487
Md. Shariful Islam Sabbir <69571889+islammdshariful@users.noreply.github.com>
2022-06-08 12:23:25 +0600

    changed driver path

--- test/conftest.py
+++ test/conftest.py
@@ -5,6 +5,7 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from pathlib import Path
 
 
 # This will run before all test case
@@ -13,7 +14,9 @@ def browser():
     opts = Options()
     # opts.add_experimental_option("detach", True)
     opts.add_experimental_option('debuggerAddress', 'localhost:9250')
-    b = webdriver.Chrome(str(os.getenv('chromedriver')), chrome_options=opts)
+    # b = webdriver.Chrome(str(os.getenv('chromedriver')), chrome_options=opts)
+    path = str(Path(__file__).parent.parent) + "\\venv\\Lib\\site-packages\\seleniumbase\\drivers\\chromedriver.exe"
+    b = webdriver.Chrome(executable_path=path, chrome_options=opts)
     b.maximize_window()
     b.implicitly_wait('10')
 
