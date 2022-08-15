6dc1ff5a6eace50b313d365b5766767d09b363d8
Md. Shariful Islam Sabbir <69571889+islammdshariful@users.noreply.github.com>
2022-08-15 16:38:08 +0600

    updates conftest

--- test/conftest.py
+++ test/conftest.py
@@ -11,7 +11,7 @@ from pathlib import Path
 def browser():
     opts = Options()
     # opts.add_experimental_option("detach", True)
-    opts.add_experimental_option('debuggerAddress', 'localhost:9250')
+    # opts.add_experimental_option('debuggerAddress', 'localhost:9250')
     if platform.system().__eq__("Windows"):
         # For Windows
         path = str(Path(__file__).parent.parent) + "/venv/Lib/site-packages/seleniumbase/drivers/chromedriver.exe"
